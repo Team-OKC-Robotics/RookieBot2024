ef4d338ef650580772f6eb2a41923072b9f311ec
Zachary <150299755+ZacharyKingery@users.noreply.github.com>
2024-11-11 19:34:27 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,86 +1,118 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+package frc.robot.subsystems;
 
-package frc.robot;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.*;
-import frc.robot.subsystems.*;
-import java.io.File;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.pathplanner.lib.commands.PathPlannerAuto;
+public class ShooterSubsystem extends SubsystemBase{
+    private CANSparkMax left_shooter_motor = new CANSparkMax(12, MotorType.kBrushless);
+    private CANSparkMax right_shooter_motor = new CANSparkMax(11, MotorType.kBrushless);
+
+    private RelativeEncoder left_encoder;
+    private RelativeEncoder right_encoder;
+
+    private SparkPIDController right_PID_controller;
+    private SparkPIDController left_PID_controller;
+
+    private double left_motor_setpoint = 0.0;
+    private double right_motor_setpoint = 0.0;
+
+    private final double PID_P = 0.0001;
+    private final double PID_FF = 0.000199;
+    private final double RPM_DIFF_THRESHOLD = 200;
+
+    private ShuffleboardTab tab = Shuffleboard.getTab("Driver");
+    private GenericEntry leftRPMEntry = tab.add("Left RPM", 0.0).getEntry();
+    private GenericEntry rightRPMEntry = tab.add("Right RPM", 0.0).getEntry();
+
+    public ShooterSubsystem() {
+        left_shooter_motor.restoreFactoryDefaults();
+        right_shooter_motor.restoreFactoryDefaults();
+
+        left_shooter_motor.setInverted(true);
+        right_shooter_motor.setInverted(false);
+
+        left_shooter_motor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        right_shooter_motor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+
+        left_encoder = left_shooter_motor.getEncoder();
+        right_encoder = right_shooter_motor.getEncoder();
+
+        left_PID_controller = left_shooter_motor.getPIDController();
+        right_PID_controller = right_shooter_motor.getPIDController();
+
+        // Ramp up the shooter motors to full speed over 1.5 seconds
+        left_shooter_motor.setClosedLoopRampRate(1.2);
+        right_shooter_motor.setClosedLoopRampRate(1.2);
+    
+        // Prevent the motors from running in reverse
+        left_PID_controller.setOutputRange(0.0, 1.0);
+        right_PID_controller.setOutputRange(0.0, 1.0);
+    
+        left_PID_controller.setFeedbackDevice(left_encoder);
+        right_PID_controller.setFeedbackDevice(right_encoder);
+
+        left_PID_controller.setP(PID_P);
+        right_PID_controller.setP(PID_P);
+
+        left_PID_controller.setFF(PID_FF);
+        right_PID_controller.setFF(PID_FF);
+    }
+
+    // TODO: Create a function that sets the speed of the shooter motors
+    // The function should have two parameters: the left shooter speed and the right shooter speed
+    // The left_motor_setpoint and right_motor_setpoint should be set and
+    // then setPIDReferences() should be called.
+    public void setShooterSpeed(double leftShooterSpeed, double rightShooterSpeed) {
+        left_motor_setpoint = leftShooterSpeed;
+        right_motor_setpoint = rightShooterSpeed;
+
+        setPIDReferences();
+    }
 
 
-public class RobotContainer
-{
 
-  final XboxController driverXbox = new XboxController(0);
-  //private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
-  //                                                                       "swerve"));
-  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    // TODO: Create a function that stops the motors. Does this function need any parameters?
+    // The left_motor_setpoint and right_motor_setpoint should be set and
+    // then setPIDReferences() should be called.
+    public void stopShooter() {
+        left_motor_setpoint = 0;
+        right_motor_setpoint = 0;;
 
-  //AbsoluteDrive absoluteDriveSlow = new AbsoluteDrive(drivebase, driverXbox, 0.4);
-  //AbsoluteDrive absoluteDriveFast = new AbsoluteDrive(drivebase, driverXbox, 0.6);
+        setPIDReferences();
+    }
 
-  private final ShootCommand shootCommand = new ShootCommand(shooterSubsystem, intakeSubsystem);
-  private final IntakeCommand intakeCommand = new IntakeCommand(shooterSubsystem, intakeSubsystem);
-  private final OutakeCommand outakeCommand = new OutakeCommand(shooterSubsystem, intakeSubsystem);
 
-  private final JoystickButton driver_a_button = new JoystickButton(driverXbox, 1);
-  private final JoystickButton driver_left_bumper_button = new JoystickButton(driverXbox, 5);
-  private final JoystickButton driver_right_bumper_button = new JoystickButton(driverXbox, 6);
-  private final JoystickButton driver_b_button = new JoystickButton(driverXbox, 2);
-  private final JoystickButton driver_x_button = new JoystickButton(driverXbox, 3);
+    // TODO: Create a function that
+    //   Returns TRUE if BOTH the left and the right motor speeds are within some tolerance of the left and right motor setpoints
+    //   Returns FALSE otherwise
+    // Hint: left_encoder.getVelocity() will get you the current speed of the left motor
+    // Hint: RPM_DIFF_THRESHOLD is the max difference between the current speed and setpoint speed we want to tolerate.
+    //   How will you handle both cases where the current speed can be above or below the setpoint?
 
-  // private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+    public boolean motorSpeedsWIthinBounds() {
+        if (left_encoder.getVelocity() > (left_motor_setpoint - RPM_DIFF_THRESHOLD) && right_encoder.getVelocity() > (right_motor_setpoint - RPM_DIFF_THRESHOLD)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-  //Makes Auto Chooser
-  private SendableChooser<String> autoChooser = new SendableChooser<String>();
-  private ShuffleboardTab tab = Shuffleboard.getTab("Driver");
+    
+    private void setPIDReferences() {
+        left_PID_controller.setReference(left_motor_setpoint, CANSparkMax.ControlType.kVelocity);
+        right_PID_controller.setReference(right_motor_setpoint, CANSparkMax.ControlType.kVelocity);
+    }
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer()
-  {
-    //Adding Auto Chooser Options
-    autoChooser.setDefaultOption("Move Forward", "Move Forward");
-    autoChooser.addOption("Move Foward after 10s", "Move Foward after 10s");
-    autoChooser.addOption("No Auto", "No Auto");
-    // autoChooser.addOption("Short Line Two", "Short Line Two");
-
-    configureBindings();
-
-    tab.add(autoChooser);
-
-    // Set default swerve drive command
-    //drivebase.setDefaultCommand(absoluteDriveSlow);
-  }
-
-  private void configureBindings()
-  {
-    //driver_a_button.onTrue((Commands.runOnce(drivebase::zeroGyro)));
-    driver_x_button.whileTrue(outakeCommand);
-    //driver_b_button.whileTrue(absoluteDriveFast);
-    driver_left_bumper_button.whileTrue(intakeCommand);
-    driver_right_bumper_button.whileTrue(shootCommand);
-  }
-
-  public void zeroGyro() {
-    //drivebase.zeroGyro();
-  }
-
-  public Command getAutonomousCommand()
-  {
-    return new PathPlannerAuto(autoChooser.getSelected());
-  }
+    @Override
+    public void periodic() {
+        leftRPMEntry.setDouble(left_encoder.getVelocity());
+        rightRPMEntry.setDouble(right_encoder.getVelocity());
+    }
 }
